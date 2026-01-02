1aa0af210c24be63f31724ddbe48ea472c748889
RosieOh <dhxogns920@gmail.com>
2026-01-02 23:50:15 +0900

    FEAT: 이미지 생성 응답 DTO 추가 - 이미지 생성 API 응답 DTO

---
+++ src/main/java/com/blaybus/infrastructure/external/image/dto/ImageGenerationResponse.java
@@ -0,0 +1,16 @@
+package com.blaybus.infrastructure.external.image.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageGenerationResponse {
+    private List<ImageData> data;
+}
+
+
