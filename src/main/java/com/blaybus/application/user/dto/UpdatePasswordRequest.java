c47ac75c85e789a6da34690d350aa3c172d4ceea
RosieOh <dhxogns920@gmail.com>
2026-01-02 23:51:16 +0900

    FEAT: 비밀번호 수정 요청 DTO 추가 - 사용자 비밀번호 수정 요청 데이터 전송 객체

---
+++ src/main/java/com/blaybus/application/user/dto/UpdatePasswordRequest.java
@@ -0,0 +1,20 @@
+package com.blaybus.application.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 비밀번호 변경 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordRequest {
+    @NotBlank(message = "현재 비밀번호는 필수입니다")
+    private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호는 필수입니다")
+    private String newPassword;
+}
