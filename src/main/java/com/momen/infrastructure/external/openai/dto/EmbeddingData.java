164237825311a2c9422a567967a4b49fddc44d44
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:39:00 +0900

    FEAT : OpenAI 임베딩 데이터 DTO 추가

---
+++ src/main/java/com/momen/infrastructure/external/openai/dto/EmbeddingData.java
@@ -0,0 +1,16 @@
+package com.momen.infrastructure.external.openai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * OpenAI Embedding 데이터 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmbeddingData {
+    private double[] embedding;
+}
+
