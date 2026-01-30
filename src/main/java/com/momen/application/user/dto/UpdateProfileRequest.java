d85cdcb1fdaee95e8b677d49882658593b44f7d6
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:38:58 +0900

    FEAT : 프로필 수정 요청 DTO 추가 - 닉네임 등

---
+++ src/main/java/com/momen/application/user/dto/UpdateProfileRequest.java
@@ -0,0 +1,16 @@
+package com.momen.application.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원 정보 수정 요청 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+    private String name;
+    private String phone;
+}
