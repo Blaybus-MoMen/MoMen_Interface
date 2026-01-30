2963758214f3d393d5342a7f8e066dd87153d3f6
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:38:57 +0900

    FEAT : Spring Boot 애플리케이션 진입점(Main) 추가 - com.momen 패키지 기동 클래스

---
+++ src/main/java/com/momen/MomenApplication.java
@@ -0,0 +1,13 @@
+package com.momen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomenApplication.class, args);
+    }
+
+}
