0dd41e815cf71161704360332533aaf9c4bdf7ff
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:38:57 +0900

    FEAT : 카카오 토큰 응답 DTO 추가 - 액세스/리프레시 토큰

---
+++ src/main/java/com/momen/application/oauth/dto/KakaoTokenResponse.java
@@ -0,0 +1,34 @@
+package com.momen.application.oauth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 카카오 토큰 응답 DTO
+ * 카카오 OAuth 서버로부터 받는 토큰 정보
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class KakaoTokenResponse {
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    private Integer refreshTokenExpiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+}
