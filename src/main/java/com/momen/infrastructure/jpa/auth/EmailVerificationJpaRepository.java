61a7e3741ed9a5d27b22847cca1a18d773f8abdb
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:39:00 +0900

    FEAT : 이메일 인증 JPA 리포지토리 추가

---
+++ src/main/java/com/momen/infrastructure/jpa/auth/EmailVerificationJpaRepository.java
@@ -0,0 +1,19 @@
+package com.momen.infrastructure.jpa.auth;
+
+import com.momen.domain.auth.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * EmailVerification JPA Repository
+ */
+public interface EmailVerificationJpaRepository extends JpaRepository<EmailVerification, Long> {
+
+    Optional<EmailVerification> findByEmailAndCode(String email, String code);
+
+    Optional<EmailVerification> findTopByEmailOrderByCreateDtDesc(String email);
+
+    List<EmailVerification> findByEmail(String email);
+}
