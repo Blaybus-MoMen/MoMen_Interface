0e87a708f79896e3af37612456e7cd941da86b43
RosieOh <dhxogns920@gmail.com>
2026-01-30 15:39:00 +0900

    FEAT : OpenAI 채팅 메시지 DTO 추가

---
+++ src/main/java/com/momen/infrastructure/external/openai/dto/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.momen.infrastructure.external.openai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * OpenAI Chat Message DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private String role;
+    private String content;
+}
+
