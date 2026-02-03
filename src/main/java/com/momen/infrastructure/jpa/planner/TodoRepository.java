4636bfcca525716060a064f2cbad8bb201785769
dietken1 <dietken1@ajou.ac.kr>
2026-02-04 03:15:56 +0900

    REFACTOR : TodoRepository 쿼리 추가

--- src/main/java/com/momen/infrastructure/jpa/planner/TodoRepository.java
+++ src/main/java/com/momen/infrastructure/jpa/planner/TodoRepository.java
@@ -2,10 +2,19 @@ package com.momen.infrastructure.jpa.planner;
 
 import com.momen.domain.planner.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByPlannerId(Long plannerId);
 
     List<Todo> findByPlannerIdIn(List<Long> plannerIds);
+
+    List<Todo> findByParentTodoId(Long parentTodoId);
+
+    @Query("SELECT t FROM Todo t WHERE t.parentTodo.id = :parentId OR t.id = :parentId")
+    List<Todo> findRecurringGroup(@Param("parentId") Long parentId);
+
+    void deleteByPlannerId(Long plannerId);
 }
