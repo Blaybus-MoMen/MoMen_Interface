dfe901b3f76d8d369fdf53a5e62ddb74e3ccab30
dietken1 <dietken1@ajou.ac.kr>
2026-02-05 02:51:20 +0900

    FEAT : Todo 엔티티 필드 구조 변경

--- src/main/java/com/momen/domain/planner/Todo.java
+++ src/main/java/com/momen/domain/planner/Todo.java
@@ -42,28 +42,26 @@ public class Todo extends BaseTimeEntity {
     @Column(name = "created_by", nullable = false)
     private Long createdBy; // 작성자 ID
 
-    @Column(name = "day_of_week", length = 10)
-    private String dayOfWeek; // MONDAY, TUESDAY, ... nullable
-
     @Column(name = "worksheet_file_url", length = 500)
     private String worksheetFileUrl; // 학습지파일 URL, nullable
 
     @Column(name = "mentor_confirmed")
     private Boolean mentorConfirmed = false; // 멘토확인여부
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_todo_id")
-    private Todo parentTodo; // 반복 그룹 추적용, nullable
+    @Column(name = "repeat_group_id", length = 36)
+    private String repeatGroupId; // 반복 그룹 UUID, nullable
+
+    @Column(name = "repeat_days", length = 100)
+    private String repeatDays; // "MONDAY,WEDNESDAY", nullable
 
     public Todo(Planner planner, String title, String subject, String goalDescription,
-                Boolean isFixed, Long createdBy, String dayOfWeek, String worksheetFileUrl) {
+                Boolean isFixed, Long createdBy, String worksheetFileUrl) {
         this.planner = planner;
         this.title = title;
         this.subject = subject;
         this.goalDescription = goalDescription;
         this.isFixed = isFixed;
         this.createdBy = createdBy;
-        this.dayOfWeek = dayOfWeek;
         this.worksheetFileUrl = worksheetFileUrl;
     }
 
@@ -80,11 +78,10 @@ public class Todo extends BaseTimeEntity {
     }
 
     public void updateContent(String title, String subject, String goalDescription,
-                              String dayOfWeek, String worksheetFileUrl, Boolean mentorConfirmed) {
+                              String worksheetFileUrl, Boolean mentorConfirmed) {
         this.title = title;
         this.subject = subject;
         this.goalDescription = goalDescription;
-        this.dayOfWeek = dayOfWeek;
         this.worksheetFileUrl = worksheetFileUrl;
         this.mentorConfirmed = mentorConfirmed;
     }
@@ -93,7 +90,13 @@ public class Todo extends BaseTimeEntity {
         this.planner = newPlanner;
     }
 
-    public void setParentTodo(Todo parent) {
-        this.parentTodo = parent;
+    public void assignRepeatGroup(String repeatGroupId, String repeatDays) {
+        this.repeatGroupId = repeatGroupId;
+        this.repeatDays = repeatDays;
+    }
+
+    public void detachFromRepeatGroup() {
+        this.repeatGroupId = null;
+        this.repeatDays = null;
     }
 }
